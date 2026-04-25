c22211ea515afaa25a7e0ad5885bf6b03097780e
oscarlondono <oscarlondono@simplesolutions.com.co>
2026-04-25 15:10:06 -0500

    Add Next.js build output and update .gitignore
    Commit adds generated Next.js build artifacts under my-agentclinic/.next (BUILD_ID, manifests, server/app pages, webpack caches/packs, prerender/routes files, etc.). Also updates my-agentclinic/.gitignore to ignore .env files and Prisma local DB files (prisma/dev.db, prisma/dev.db-journal). These are build-generated artifacts and env/database local files.

---
+++ my-agentclinic/.next/static/C8raUv9RZjcWTtb33puWH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-fc6086a10d77b8b7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
